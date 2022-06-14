e1942879bfc571828a6bb3070430759e68389cf9
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-06-14 15:41:00 +0300

    complited task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(e => e > 0).filter(e => e % 3 === 0).map(e => e * 10);
+  return resultArr;
 }
