6a04026005cb82d1aabcd885e9c829b2b54c9c87
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-06-17 15:29:46 +0300

    complited task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name,
+  this.gender = gender,
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks != undefined) {
+    this.marks.push(mark);
+  } else {
+    this.marks = [mark];
+  };
+}
+
+Student.prototype.addMarks = function (...args) {
+  if (this.marks != undefined) {
+    this.marks.push(...args);
+  } else {
+    this.marks = [...args];
+  };
+}
+
+Student.prototype.getAverage = function () {
+  if (this.marks != undefined) {
+    return this.marks.map(i=>x+=i, x=0).reverse()[0] / this.marks.length;
+  } else {
+    console.log("not marks");
+    return null;
+  };
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
