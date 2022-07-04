d04a28e33718eebed80bfc2f7c2e440ed17db798
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-07-04 18:45:17 +0300

    review

---
+++ 7.async/task.js
@@ -0,0 +1,60 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.timerId = null;
+    }
+    
+    addClock(time, callback, id) {
+        if(id === undefined) {
+            throw new Error('id не передан');
+        }
+        let obj = this.alarmCollection.find(element => element.id === id);
+        if(obj != undefined) {
+            console.error("Будильник с таким id уже существует");
+        } else {
+            this.alarmCollection.push({id, time, callback});
+        }
+    }
+
+    removeClock(id) {
+        let temp = this.alarmCollection.filter(item => item.id != id);
+        this.alarmCollection = temp;
+    }
+
+    getCurrentFormattedTime() {
+        return new Date().toLocaleTimeString().slice(0,-3);
+    }
+
+    start() {
+        if(this.timerId === null) {
+            this.timerId = setInterval(() => this.alarmCollection.forEach(element => {() => checkClock(element)}));
+        }
+    }
+
+    checkClock(element) {
+        if(element.time === (() => this.getCurrentFormattedTime())) {
+            element.callback();
+        }
+    }
+
+    stop() {
+        if(this.timerId != null) {
+            clearInterval();
+            this.timerId = null;
+        }
+    }
+
+    printAlarms() {
+        console.log(`Печать всех будильников в количестве ${this.alarmCollection.length}`);
+        this.alarmCollection.forEach(element => {
+            console.log(`будильник №${element.id} заведен на ${element.time}`);
+        });
+    }
+
+    clearAlarms() {
+        this.printAlarms();
+        this.stop();
+        this.alarmCollection = [];
+        this.printAlarms();
+    }
+}
