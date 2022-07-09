0ca0bec7a0c6a98a7179871433b00c9ce00a6e2b
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-07-09 13:27:06 +0300

    review

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,62 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  
+  function wrapper(...args) {
+      const hash = args.join(',');
+      let objectInCache = cache.find(item => item.hash === hash);
+      if (objectInCache) {
+          console.log("Из кэша: " + objectInCache.value);
+          return "Из кэша: " + objectInCache.value;
+      }
+  
+      let result = func(...args);
+      cache.push({hash: hash, value: result}) ;
+      if (cache.length > 5) { 
+        cache.shift(); 
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  }
+  return wrapper;
+  }
+
+function debounceDecoratorNew(func, delay) {
+  let isTrottled = false;
+  return function() {
+    if(isTrottled) {
+      return;
+    }
+    func();
+    isTrottled = true;
+    setTimeout(() => isTrottled = false, delay);
+  }
 }
 
+function debounceDecorator2(func, delay) {
+  let isTrottled = false;
+  let count = null;
+  return function() {
+    count++;
+    if(isTrottled) {
+      return;
+    }
+    function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.find(item => item.hash === hash);
+    if (objectInCache) {
+        console.log("Из кэша: " + objectInCache.value);
+        return "Из кэша: " + objectInCache.value;
+    }
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
+    let result = func(...args);
+    cache.push({hash: hash, value: result, count: count}) ;
+    if (cache.length > 5) { 
+      cache.shift(); 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+    }
+    isTrottled = true;
+    setTimeout(() => isTrottled = false, delay);
+  }
 }
