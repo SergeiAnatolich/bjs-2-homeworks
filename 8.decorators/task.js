9e900fbb86540b27ffaa898e5f6b5639c9724ed6
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-07-14 18:06:13 +0300

    complited task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -18,45 +18,43 @@ function cachingDecoratorNew(func) {
       return "Вычисляем: " + result;  
   }
   return wrapper;
-  }
+}
 
 function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
   let isTrottled = false;
   return function() {
-    if(isTrottled) {
+    clearTimeout(timeoutId);
+    if (isTrottled) {
+      timeoutId = setTimeout(() => {
+        isTrottled = false;
+        func();
+      }, delay);
       return;
     }
     func();
     isTrottled = true;
-    setTimeout(() => isTrottled = false, delay);
+    timeoutId = setTimeout(() => isTrottled = false, delay);
   }
 }
 
 function debounceDecorator2(func, delay) {
+  let timeoutId = null;
   let isTrottled = false;
-  let count = null;
-  return function() {
-    count++;
-    if(isTrottled) {
+  function wrapper() {
+    wrapper.count++;
+    clearTimeout(timeoutId);
+    if (isTrottled) {
+      timeoutId = setTimeout(() => {
+        isTrottled = false;
+        func();
+      }, delay);
       return;
     }
-    function wrapper(...args) {
-    const hash = args.join(',');
-    let objectInCache = cache.find(item => item.hash === hash);
-    if (objectInCache) {
-        console.log("Из кэша: " + objectInCache.value);
-        return "Из кэша: " + objectInCache.value;
-    }
-
-    let result = func(...args);
-    cache.push({hash: hash, value: result, count: count}) ;
-    if (cache.length > 5) { 
-      cache.shift(); 
-    }
-    console.log("Вычисляем: " + result);
-    return "Вычисляем: " + result;  
-    }
+    func();
     isTrottled = true;
-    setTimeout(() => isTrottled = false, delay);
+    timeoutId = setTimeout(() => isTrottled = false, delay);
   }
+  wrapper.count = 0;
+  return wrapper;
 }
